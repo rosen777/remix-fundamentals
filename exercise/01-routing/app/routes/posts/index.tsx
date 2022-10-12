27be89d7aff51064436ec4c9871d9aec4ec255f9
Rosen Toshev <rtoscheff@gmail.com>
2022-10-12 01:24:34 +0100

    Added the posts route

---
+++ exercise/01-routing/app/routes/posts/index.tsx
@@ -0,0 +1,10 @@
+import { Form, Link } from "@remix-run/react";
+import * as React from "react";
+
+export default function Posts() {
+  return (
+    <main>
+      <h1>Posts</h1>
+    </main>
+  );
+}
